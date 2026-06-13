2e3439d196241557d45b513f9fd9488c0a67db80
sethulakshmi2428 <sethulakshmivs2k03@gmail.com>
2026-06-13 22:44:33 +0530

    schema change

--- Backend/Models/userSchema.js
+++ Backend/Models/userSchema.js
@@ -33,7 +33,7 @@ const userSchema = new mongoose.Schema({
     },
     role: {
         type: String,
-        enum: ["user", "admin","officials"],
+        enum: ["user", "admin","official"],
         default: "user"
     }
 })
